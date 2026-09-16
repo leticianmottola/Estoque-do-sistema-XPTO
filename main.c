329bbcd4c3e510b8bbeb5ff8af63ffcce5ab2bb2
Letícia Mottola <leticianunes5417@gmail.com>
2026-09-16 20:52:13 -0300

    main

--- main.c
+++ main.c
@@ -2,18 +2,22 @@
 
 int main()
 {
-    FILE *pointer;
+    FILE *estoque; // Aponta aonde o arquivo está guardado na memória.
 
-    pointer = fopen("storage.txt", "r");
-
-    if (pointer == NULL) 
+    estoque = fopen("storage.txt", "r"); /* Função chamada para abrir o arquivo "storage.txt", o arquivo do estoque.
+    O "r" demonstra apenas a leitura deste arquivo. */
+    if (estoque == NULL)  // Trava de segurança.
     {
         printf("Erro ao abrir o arquivo. \n");
     }
     else 
     {
         printf("Abriu o arquivo.");
-        fclose(pointer);
+        while(feof(estoque) == 0) 
+        {
+            fscanf(arquivo, "%d%d", &a, &b);
+        }
+        fclose(estoque);
     }
 
 }
